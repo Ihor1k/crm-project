b3bf4c7a94fe78c0a89296cda256b2fc420d7f4a
Ihor1k <igorchik2006@gmail.com>
2026-04-17 11:53:58 +0200

    Updates

--- assets/dashboard-legacy-Bcxes3Gx.js
+++
@@ -1,1 +0,0 @@
-System.register(["./DashboardPage-legacy-Dr7dsmiz.js"],(function(e,t){"use strict";var n;return{setters:[e=>{n=e.D}],execute:function(){const e=document.getElementById("app");n().mount(e)}}}));
