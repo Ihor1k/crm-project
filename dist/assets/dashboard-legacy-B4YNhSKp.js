c52d392342e068d759c7fca9f743a9008e4e31a5
Ihor1k <igorchik2006@gmail.com>
2026-04-21 13:40:33 +0200

    change width for button

---
+++ dist/assets/dashboard-legacy-B4YNhSKp.js
@@ -0,0 +1,1 @@
+System.register(["./DashboardPage-legacy-Dm3PIo37.js"],(function(e,t){"use strict";var n;return{setters:[e=>{n=e.D}],execute:function(){const e=document.getElementById("app");n().mount(e)}}}));
